25f1cddf90450820a4e60204d3b2a28e49f32588
Dane Kennedy <kennedy.dane@gmail.com>
2025-07-30 17:47:34 +0200

    Use caddy to proxy to minio

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "minio_server" {
     image_name      = var.server_image
     flavor_name     = var.server_flavor
     key_pair        = openstack_compute_keypair_v2.terraform-key.name
-    security_groups = ["default", openstack_networking_secgroup_v2.minio_security.name]
+    security_groups = [openstack_networking_secgroup_v2.minio_security.name]
 
     # network {
     #   name = openstack_networking_network_v2.minio_network.name
